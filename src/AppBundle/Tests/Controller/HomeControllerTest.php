6711a70724c55ab0465226a84a94765fa0f43c0b
mike <x@m>
2016-09-30 21:03:09 +0200

    fornt, tests functional

---
+++ src/AppBundle/Tests/Controller/HomeControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare (strict_types=1);
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GameControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+}
